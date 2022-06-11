02b2a19f622b4cce63aed01d6bf8258fb36cba55
SnurAulia <snuraulia48@gmail.com>
2022-06-11 10:21:11 +0700

    Commit1

--- resources/views/Groups/index.blade.php
+++ resources/views/Groups/index.blade.php
@@ -1,8 +1,7 @@
 @extends('layout.app')
-
 @section('title','Groups')
-
 @section('content')
+
 <a href="/groups/create" class="card-link btn-primary">Tambah Group</a>
 @foreach($groups as $group)
 <div class="card" style="width: 18rem;">
@@ -12,19 +11,17 @@
     <hr>
     <a href="/groups/addmember/{{ $group->id }}" class="card-link btn-primary">Tambah Anggota Teman</a>
     <ul class="list-group">  
-
   @foreach ($group->friends as $friend)
-    <li class="list-group-item d-flex justify-content-between align-items-start">
-     {{$friend->nama}}
-    <form action="/groups/deleteaddmember{{ $friend->id }}" method="POST">
-      @csrf
-      @method('PUT')
-    <button type="submit" class="bedge card-link btn-danger">X</button>
+  <li class="list-group-item d-flex justify-content-between align-items-start">
+    {{$friend->nama}}
+    <form action="/groups/deleteaddmember/{{ $friend->id }}" method="POST">
+    @csrf
+    @method('PUT')
+    <button type="submit" class="btn btn-danger">X</button>
     </form>
-    </li>
-  @endforeach
+  </li>
+@endforeach
     </ul>
-
     <hr>
     <a href="/groups/{{ $group['id'] }}/edit" class="card-link btn-warning">Edit Groups</a>
     <form action="/groups/{{ $group['id'] }}" method="POST">
@@ -34,8 +31,8 @@
 </form>
 </div>
 </div>
-
 @endforeach
+
 <div>
   {{ $groups->links() }}
 </div>
